48f7a010d16322fdc7611058fcf5ec35f18e40cb
Andre Busche <busche@ismll.de>
2014-05-01 17:14:55 +0200

    added function get_xmx

--- scripts/sge/memlib.sh
+++ scripts/sge/memlib.sh
@@ -21,6 +21,28 @@ function get_mem() {
         echo $ret
 }
 
+function get_xmx() {
+#       echo "calculating memory for $1 at a required minimum of $2"
+        ret=""
+        while read line; do
+
+        	#read the line
+        	read key value <<<$line
+       		if [ $value -gt $2 ]; then
+	        	ret=$key
+                	break
+        	fi
+
+        done < <(cat $1) # of while
+
+        if [ 'x'$ret = 'x' ]; then
+                # fallback if no mem value was found.
+                ret=130
+        fi
+        echo $ret
+}
+
+
 #mem=$(get_mem "$1" $2)
 #echo mem is $mem
 
