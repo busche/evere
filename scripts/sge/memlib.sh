b0626b221f428d879ac739bf67f28efa2d325c00
Andre Busche <busche@ismll.de>
2014-05-01 17:24:48 +0200

    added documentation and docu reference

--- scripts/sge/memlib.sh
+++ scripts/sge/memlib.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#
+#  utility functions for calculating memory request and constraints between ulimit and Xmx
+#
+# one may use get_mem to 'forward-calculate' from Xmx to an ulimit value, and
+# get_xmx to 'backward-calculate' from an ulimit value to an approximate suitable Xmx value.
+#
+# both functions take two arguments:
+#  (1) the lookup file name
+#  (2) the xmx / ulimit value to be sougt in the file
+#
+# see the document memory_in_sge for further information.
+#
+
 function get_mem() {
 #       echo "calculating memory for $1 at a required minimum of $2"
         ret=""
@@ -18,7 +31,7 @@ function get_mem() {
                 # fallback if no mem value was found.
                 ret=130
         fi
-        echo $ret
+        echo $((ret+2))
 }
 
 function get_xmx() {
@@ -39,7 +52,7 @@ function get_xmx() {
                 # fallback if no mem value was found.
                 ret=130
         fi
-        echo $ret
+        echo $((ret-2))
 }
 
 
